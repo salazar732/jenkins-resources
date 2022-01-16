fc8fab219ba8f674410bcf66408e62e6d631e7c1
Samuel Mayers <36217260+salazar732@users.noreply.github.com>
2022-01-16 18:59:28 -0400

    Update put.sh

--- jenkins/jenkins-ansible/put.sh
+++ jenkins/jenkins-ansible/put.sh
@@ -9,6 +9,6 @@ while [ $counter -lt 50 ]; do
   lastname=$(nl people.txt | grep -w $counter | awk '{print $2}' | awk -F ',' '{print $2}')
   age=$(shuf -i 20-25 -n 1)
 
-  mysql -u root -p1234 people -e "insert into register values ($counter, '$name', '$lastname', $age)"
+  mysql -u root -p1234 people -e "insert into registro values ($counter, '$name', '$lastname', $age)"
   echo "$counter, $name $lastname, $age was correctly imported"
 done
